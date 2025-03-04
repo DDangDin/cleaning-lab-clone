414a4f3def005d6be5640370f7f25331516e2fed
DDangDin <Kcorelec7123@gmail.com>
2025-03-04 17:47:14 +0900

    feat: Typography 추가
    - pretendard 폰트 3종 추가

---
+++ designsystem/src/main/java/com/example/designsystem/theme/theme/Type.kt
@@ -0,0 +1,21 @@
+package com.example.designsystem.theme.theme
+
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import com.example.designsystem.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+)
+
+val lineHeightStyle = LineHeightStyle(
+    alignment = LineHeightStyle.Alignment.Center,
+    trim = LineHeightStyle.Trim.None,
+)
+val platformTextStyle = PlatformTextStyle(includeFontPadding = false)
